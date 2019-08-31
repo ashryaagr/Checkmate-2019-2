094a57bbbed089023c8ed776c266d60ba9964bbb
ashryaagr <ashryaagr@gmail.com>
2019-08-31 17:32:08 +0530

    registration page and login page integrated with backend

--- public/js/login.js
+++ public/js/login.js
@@ -3,16 +3,18 @@ $("#submit").click(function (event) {
 	$("#submit").val('Verifying...') ;
 	$("#submit").attr('disabled','disabled');
 	const body = {
-		username: $("#username").val(),
-		password: $("#password").val(),
+		"query" : "mutation { "+
+		"login (username : \"" + $("#username").val() + "\", password : \""+ $("#password").val() + "\""+
+			" }"
 	};
+	// TODO: This AJAX Call returns a JWT token on success, which is to be stored in a browser cookie
 	$.ajax({
 		url: "/login",
 		method: 'POST',
 		data: body,
 		success: function(){
 			alert("Successfuly logged in")
-			window.location.href = window.location.origin + "/add_friend"
+			window.location.href = window.location.origin + "/game"
 		},
 		error : function () {
 			alert("Invalid credentials")
--- public/js/register.js
+++ public/js/register.js
@@ -1,26 +1,27 @@
 $("#submit").click(function (event) {
 	event.preventDefault();
-	$("#submit").val('Adding...')
+	$("#submit").val('Registering...')
 	$("#submit").attr('disabled','disabled');
-	const body = {
+	const details = {
 		username: $("#username").val(),
 		password: $("#password").val(),
-		name: $("#name").val(),
-		stack: $("#stack").val(),
-		reddit : $("#reddit").val(),
-		twitter : $("#twitter").val(),
-		youtube : $("youtube").val()
+		id_1: $("#id_1").val(),
+		id_2: $("#id_2").val()
 	};
+	const body = {
+		"query": "mutation register( input : { " + JSON.stringify(details) + " })"
+	};
+	// TODO: This AJAX Call returns a JWT token on success, which is to be stored in a browser cookie
 	$.ajax({
 		url: "/user",
 		method: 'POST',
 		data: body,
 		success: function(){
-			alert("Successfuly registered user")
-			window.location.href = window.location.origin + "/login"
+			alert("Successfuly registered user") ;
+			window.location.href = window.location.origin + "/game"
 		},
 		error : function () {
-			alert("Invalid data/username/ids")
+			alert("Invalid data/username/ids") ;
 			document.getElementById("submit").disabled = false ;
 			$("#submit").val("Submit")
 		}
